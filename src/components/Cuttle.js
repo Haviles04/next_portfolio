bfe61944276bb20cf1230e6b38306e0683554619
Henry Aviles <haviles04@gmail.com>
2024-03-23 09:49:56 -0400

    chore: removed unneeded code

--- src/components/Cuttle.js
+++ src/components/Cuttle.js
@@ -1,23 +1,10 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import styles from "@/styles/cuttle.module.css";
-import { useScroll } from "./hooks/ScrollContextProvider";
 import CuttleLogo from "../assets/cuttleLogo.png";
 import Image from "next/image";
 import GITHUB from "@/assets/icons8-github-240.png";
 
 function Cuttle() {
-  const { scrollY } = useScroll();
-  const [scaleAmt, setScaleAmt] = useState(0);
-  const [widthAmt, setWidthAmt] = useState(0);
-
-  useEffect(() => {
-    const scrollAmt = scrollY - window.innerHeight * 7;
-    setScaleAmt(Math.min(Math.max(scrollAmt * 0.0001 + 1, 1), 1.1));
-    setWidthAmt(Math.min(Math.max(scrollAmt * 0.1, 0), 100));
-  }, [scrollY]);
-
-  console.log(scaleAmt);
 
   return (
     <section className={styles.container} id="cuttle">
