9ece6438875710000d06f1d6d34500744f7b3b72
Henry Aviles <haviles04@gmail.com>
2024-04-13 09:31:20 -0400

    fix: updated flex direction

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,25 +9,27 @@ function Contact() {
   return (
     <section className={styles.contact} id="contact">
       <h1>Lets get in touch!</h1>
-      <a href="mailto:haviles04@gmail.com">
-        <Image src={GMAIL} alt="gmail logo" width={200} />
-      </a>
-      <a
-        href="https://github.com/Haviles04"
-        target="blank"
-        rel="noopener noreferrer"
-      >
-        <div className={styles.githubBg}>
-          <Image src={GITHUB} alt="github logo" width={200} />
-        </div>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/henry-aviles-4b020a252/"
-        target="blank"
-        rel="noopener noreferrer"
-      >
-        <Image src={LINKEDIN} alt="LinkedIn logo" width={200} />
-      </a>
+      <div className={styles.iconContainer}>
+        <a href="mailto:haviles04@gmail.com">
+          <Image src={GMAIL} alt="gmail logo" width={200} />
+        </a>
+        <a
+          href="https://github.com/Haviles04"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <div className={styles.githubBg}>
+            <Image src={GITHUB} alt="github logo" width={200} />
+          </div>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/henry-aviles-4b020a252/"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <Image src={LINKEDIN} alt="LinkedIn logo" width={200} />
+        </a>
+      </div>
     </section>
   );
 }
